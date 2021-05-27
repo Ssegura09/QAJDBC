3db1fb28acc99be18f8fa07e46a1edf1ff99450a
Stephanie Segura <66101450+Ssegura09@users.noreply.github.com>
2021-05-27 11:09:43 -0500

    Update DBconfig.java

--- src/main/java/com/qa/utils/DBconfig.java
+++ src/main/java/com/qa/utils/DBconfig.java
@@ -4,6 +4,6 @@ public class DBconfig {
 	//Not ideal for security purposes
 	public static String user = "root";
 	//password(pw) is exposed
-	public static String pw = "root861612!2";
+	public static String pw = ""; //ommitted for security purposes
 	public static String url = "jdbc:mysql://localhost:3306/scanner_example?db_name&serverTimezone=UTC";
 }
